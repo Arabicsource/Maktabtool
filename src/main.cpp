e250c60e2405ef29e32504e2fa158677c6703fe8
shaybix <shaybix@tuta.io>
2018-03-08 01:51:20 +0000

    open Access compatible file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 #include "File.h"
+#include<mdb/mdbtools.h>
 
 
-int main()
+int main(int argc, char* argv )
 {
-   File* f;
 
-   return 0;
+    // mdb file handle
+    MdbHandle *mdb;
 
+
+
+    mdb = mdb_open (argv[1], MDB_NOFLAGS);
+    if (!mdb) {
+	std::cout << "Could not open file" << std::endl;
+	return 1;
+    }
+
+
+    
+    return 0; 
 }
 
 
